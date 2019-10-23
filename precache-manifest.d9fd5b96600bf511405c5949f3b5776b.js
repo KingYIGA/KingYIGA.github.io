95e6d954c464c927ae01fa1a3e6a08d097a325d7
KingYIGA <kingyinggang@163.com>
2019-10-23 16:05:55 +0800

    feat:build

---
+++ precache-manifest.d9fd5b96600bf511405c5949f3b5776b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d46847cbc8c81d36c4e275a51c5fc55",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c56d0b1d0b2716801264",
+    "url": "/static/css/2.23286d3e.chunk.css"
+  },
+  {
+    "revision": "5764940a0bfa623c62db",
+    "url": "/static/css/main.dfca195d.chunk.css"
+  },
+  {
+    "revision": "c56d0b1d0b2716801264",
+    "url": "/static/js/2.6a44cf5c.chunk.js"
+  },
+  {
+    "revision": "5764940a0bfa623c62db",
+    "url": "/static/js/main.fb638f42.chunk.js"
+  },
+  {
+    "revision": "fe23eb08f7b6157fad63",
+    "url": "/static/js/runtime-main.e41aa484.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
